bb983d26c03c9d1c8ed2ad1536d0603eba5cf201
lkvu <lieukhacvu.mail@gmail.com>
2019-10-18 10:59:32 +0700

    change variables name

--- algorithm/insertsort.cpp
+++ algorithm/insertsort.cpp
@@ -1,28 +1,27 @@
-
 #include <iostream>
 #include <vector>
 int main(){
-  std::vector<int> vecnum;
+  std::vector<int> seq;
   std::cout << "Enter sequence of int number: ";
   int num;
   while(std::cin >> num){
-	vecnum.push_back(num);
+	seq.push_back(num);
   }
   
-  for (int i=1; i<vecnum.size(); ++i){
-	int key{vecnum[i]};
-	int j{i - 1};
-	while (j>=0 && vecnum[j]>key){
-	  vecnum[j+1] = vecnum[j];
-	  vecnum[j]=key;
-	  key = vecnum[j];
-	  --j;
+  for (int key_pos=1; key_pos<seq.size(); ++key_pos){
+	int key{seq[key_pos]};
+	int insert_pos{key_pos - 1};
+	while (insert_pos>=0 && seq[insert_pos]>key){
+	  seq[insert_pos+1] = seq[insert_pos];
+	  seq[insert_pos]=key;
+	  key = seq[insert_pos];
+	  --insert_pos;
 	}
   }
   
-  std::cout << "The number sorted: ";
-  for (auto n : vecnum)
-	std::cout << n << " ";
+  std::cout << "Sequence sorted: ";
+  for (auto ele : seq)
+	std::cout << ele << " ";
   std::cout << std::endl;
   return 0;
 }
