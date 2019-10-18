bfb5ae7c6db4578d5da2cbb88cda5bae3f6ce676
infidicos <infidicos@gmail.com>
2019-10-18 11:19:36 +0700

    add comment

--- algorithm/insertsort.cpp
+++ algorithm/insertsort.cpp
@@ -1,3 +1,4 @@
+//increment insertsort algorithm
 #include <iostream>
 #include <vector>
 int main(){
@@ -7,18 +8,19 @@ int main(){
   while(std::cin >> num){
 	seq.push_back(num);
   }
-  
+  //insertsort algorithm core
   for (int key_pos=1; key_pos<seq.size(); ++key_pos){
 	int key{seq[key_pos]};
 	int insert_pos{key_pos - 1};
 	while (insert_pos>=0 && seq[insert_pos]>key){
+	  //swap key to new positon
 	  seq[insert_pos+1] = seq[insert_pos];
 	  seq[insert_pos]=key;
 	  key = seq[insert_pos];
 	  --insert_pos;
 	}
   }
-  
+  //==============================================
   std::cout << "Sequence sorted: ";
   for (auto ele : seq)
 	std::cout << ele << " ";
