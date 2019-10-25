1fdac3d1da65f0216d5f7d82018a4e3238d140ab
infidicos <infidicos@gmail.com>
2019-10-25 18:10:14 +0700

    primmer

--- primmer/sale_data.cpp
+++ primmer/sale_data.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #incluse <string>
 using namespace std;
+struct sale_data{
+  string isbn() const {return bookNo;};
+  sale_data combine(sale_data sd1, sale_data sd2);
+  double avr_sale();
+  string bookNo;
+  double revenue;
+  unsigned units_sold;
+}
