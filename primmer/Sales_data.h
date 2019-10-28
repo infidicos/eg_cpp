33332194a9c34ef052ea5bd1260dd0a3665ec00b
infidicos <infidicos@gmail.com>
2019-10-28 21:50:56 +0700

    class Sale_data

--- primmer/Sales_data.h
+++ primmer/Sales_data.h
@@ -1,71 +1,37 @@
-/*
- * This file contains code from "C++ Primer, Fifth Edition", by Stanley B.
- * Lippman, Josee Lajoie, and Barbara E. Moo, and is covered under the
- * copyright and warranty notices given in that book:
- * 
- * "Copyright (c) 2013 by Objectwrite, Inc., Josee Lajoie, and Barbara E. Moo."
- * 
- * 
- * "The authors and publisher have taken care in the preparation of this book,
- * but make no expressed or implied warranty of any kind and assume no
- * responsibility for errors or omissions. No liability is assumed for
- * incidental or consequential damages in connection with or arising out of the
- * use of the information or programs contained herein."
- * 
- * Permission is granted for this code to be used for educational purposes in
- * association with the book, given proper citation if and when posted or
- * reproduced.Any commercial use of this code requires the explicit written
- * permission of the publisher, Addison-Wesley Professional, a division of
- * Pearson Education, Inc. Send your request for permission, stating clearly
- * what code you would like to use, and in what specific way, to the following
- * address: 
- * 
- * 	Pearson Education, Inc.
- * 	Rights and Permissions Department
- * 	One Lake Street
- * 	Upper Saddle River, NJ  07458
- * 	Fax: (201) 236-3290
-*/ 
+#ifndef sales_data_h
+#define sales_data_h
 
-#ifndef SALES_DATA_H
-#define SALES_DATA_H
-
-#include <string>
 #include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
 
-class Sales_data {
-friend Sales_data add(const Sales_data&, const Sales_data&);
-friend std::ostream &print(std::ostream&, const Sales_data&);
-friend std::istream &read(std::istream&, Sales_data&);
-public:
-	// constructors
-	Sales_data(): units_sold(0), revenue(0.0) { }
-	Sales_data(const std::string &s): 
-	           bookNo(s), units_sold(0), revenue(0.0) { }
-	Sales_data(const std::string &s, unsigned n, double p):
-	           bookNo(s), units_sold(n), revenue(p*n) { }
-	Sales_data(std::istream &);
-
-	// operations on Sales_data objects
-	std::string isbn() const { return bookNo; }
-	Sales_data& combine(const Sales_data&);
-	double avg_price() const;
-private:
-	std::string bookNo;
-	unsigned units_sold;
+class Sales_data{
+	string bookNo;
+	int unitsSold;
 	double revenue;
+public:
+	Sales_data(): bookNo{""}, unitsSold{0}, revenue{0.0} {};
+	Sales_data(string str): bookNo{str} {};
+	Sales_data(string str, int num, double rev): bookNo{str}, unitsSold{num}, revenue{rev} {};
+	string isbn();
+	Sales_data& combine(const Sales_data&);
+	int totalSold();
+	double avgPrice();
+	double getRevenue();
 };
 
-
-// nonmember Sales_data interface functions
-Sales_data add(const Sales_data&, const Sales_data&);
-std::ostream &print(std::ostream&, const Sales_data&);
-std::istream &read(std::istream&, Sales_data&);
-
-// used in future chapters
-inline 
-bool compareIsbn(const Sales_data &lhs, const Sales_data &rhs)
-{
-	return lhs.isbn() < rhs.isbn();
+string Sales_data::isbn(){return bookNo;}
+Sales_data& Sales_data::combine(const Sales_data& trans){
+	if (bookNo == trans.bookNo){
+		unitsSold += trans.unitsSold;
+		revenue += trans.revenue;
+	}
+	return *this;
 }
+int Sales_data::totalSold(){return unitsSold;}
+double Sales_data::avgPrice(){return unitsSold==0 ? 0 : revenue/unitsSold;}
+double Sales_data::getRevenue(){return revenue;}
 #endif
