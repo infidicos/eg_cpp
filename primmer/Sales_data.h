39136d72960cfdb4acf5ea570914479e0f04bc75
infidicos <infidicos@gmail.com>
2019-10-29 12:23:22 +0700

    friend

--- primmer/Sales_data.h
+++ primmer/Sales_data.h
@@ -7,6 +7,8 @@ using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
+using std::istream;
+using std::ostream;
 
 class Sales_data{
 	string bookNo;
@@ -16,22 +18,43 @@ public:
 	Sales_data(): bookNo{""}, unitsSold{0}, revenue{0.0} {};
 	Sales_data(string str): bookNo{str} {};
 	Sales_data(string str, int num, double rev): bookNo{str}, unitsSold{num}, revenue{rev} {};
-	string isbn();
-	Sales_data& combine(const Sales_data&);
-	int totalSold();
+	string isbn() const;
+	Sales_data combine(const Sales_data&);
+	int totalUnitsSold();
 	double avgPrice();
 	double getRevenue();
+	friend Sales_data add(const Sales_data&, const Sales_data&);
 };
 
-string Sales_data::isbn(){return bookNo;}
-Sales_data& Sales_data::combine(const Sales_data& trans){
-	if (bookNo == trans.bookNo){
+string Sales_data::isbn() const {return bookNo;}
+Sales_data Sales_data::combine(const Sales_data& trans){
+	if (isbn() == trans.isbn()){
 		unitsSold += trans.unitsSold;
 		revenue += trans.revenue;
 	}
 	return *this;
 }
-int Sales_data::totalSold(){return unitsSold;}
+int Sales_data::totalUnitsSold(){return unitsSold;}
 double Sales_data::avgPrice(){return unitsSold ? revenue/unitsSold : 0;}
 double Sales_data::getRevenue(){return revenue;}
+
+Sales_data add(const Sales_data& item1, const Sales_data& item2){
+		Sales_data resultAdd;
+	if (item1.isbn() == item2.isbn()){
+		resultAdd.bookNo = item1.isbn();
+		resultAdd.unitsSold = item1.unitsSold + item2.unitsSold;
+		resultAdd.revenue = item1.revenue + item2.revenue;
+	}
+	return resultAdd;
+}
+/*
+istream read(istream is, Sales_data& itemIn){
+
+	return is;
+}
+
+ostream print(const Sales_data& itemOut){
+	return cout << itemOut.isbn();
+}
+*/
 #endif
